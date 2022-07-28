e19fd29fc01c5e467ca00d54053c8faa3304b926
almudenaftourne <almudena.ftourne@gmail.com>
2022-07-28 15:29:19 +0200

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -54,7 +54,9 @@ def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # return model
     return model
 
+
 def parse_args():
+    
     # setup arg parser
     parser = argparse.ArgumentParser()
 
@@ -69,6 +71,7 @@ def parse_args():
 
     # return args
     return args
+
 
 # run script
 if __name__ == "__main__":
